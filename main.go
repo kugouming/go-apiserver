3225ca31c8f195e95e4f6bae0145bafe6323dba8
kugouming <kugouming@sina.com>
2018-12-25 00:47:37 +0800

    conf loading plugin: viper

--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"go-apiserver/config"
 	"go-apiserver/router"
 	"log"
 	"net/http"
-	"github.com/gin-gonic/gin"
 	"time"
 )
 
-var port = ":8080"
+var (
+	addr = ":8080"
+	cfg = pflag.StringP("config", "c", "",  "Apiserver config file path.")
+)
 
+func main() {
+	// 解析命令行参数
+	pflag.Parse()
 
-func main()  {
+	// init config.
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(viper.GetString("addr"))
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
+	addr = viper.GetString("addr")
+	log.Print("run mode:", viper.GetString("runmode"))
+
 	// Create the Gin engine.
 	g := gin.New()
 
@@ -33,15 +54,15 @@ func main()  {
 		log.Println("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", port)
-	log.Printf(http.ListenAndServe(port, g).Error())
+	log.Printf("Start to listening the incoming requests on http address: %s", addr)
+	log.Printf(http.ListenAndServe(addr, g).Error())
 }
 
 // pingServer pings the http server to make sure the router is working.
 func pingServer() error  {
 	for i := 0; i < 3; i++ {
 		// Ping the server by sending a GET request to `/health`.
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
